4b71d7d8fe29ad05dcf557bcfd4be59fdb7b92af
mosop <mosop@users.noreply.github.com>
2017-01-23 00:23:02 +0900

    doc [ci skip]

--- src/cli.cr
+++ src/cli.cr
@@ -1,14 +1,17 @@
 require "./lib"
 
 module Cli
+  # :nodoc:
   def self.env
     ENV["CRYSTAL_CLI_ENV"]?
   end
 
+  # :nodoc:
   def self.test?
     env == "test"
   end
 
+  # :nodoc:
   def self.new_default_io
     IoHash.new.tap do |io|
       io[:out] = if test?
