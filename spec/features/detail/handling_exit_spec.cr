f3f706844a7bd467ab52772f4c3fdc07c055d7ec
mosop <mosop@users.noreply.github.com>
2018-06-08 00:58:00 +0900

    conforms to Crystal 0.25

--- spec/features/detail/handling_exit_spec.cr
+++ spec/features/detail/handling_exit_spec.cr
@@ -3,7 +3,7 @@ require "../../spec_helper"
 module CliHandlingExitFeatureDetail
   include Cli::Spec::Helper
 
-  macro test(run, code = 0, out = nil, err = nil)
+  macro test(run, code = 0, output = nil, err = nil)
     class Exit%x < Cli::Command
       class Help
         title "title"
@@ -14,19 +14,19 @@ module CliHandlingExitFeatureDetail
     end
     describe name do
       it {{run}} do
-        Exit%x.run.should exit_command(code: {{code}}, output: {{out}}, error: {{err}})
+        Exit%x.run.should exit_command(code: {{code}}, output: {{output}}, error: {{err}})
       end
     end
   end
 
   test %(exit!)
-  test %(exit! "message"), out: "message"
+  test %(exit! "message"), output: "message"
   test %(exit! "message", error: true), err: "message", code: 1
   test %(exit! "message", code: 22), err: "message", code:22
   test %(error! "message"), err: "message", code: 1
   test %(error! "message", code: 22), err: "message", code: 22
-  test %(help!), out: "title"
+  test %(help!), output: "title"
   test %(help! "message"), err: "message\n\ntitle", code: 1
   test %(help! "message", code: 22), err: "message\n\ntitle", code: 22
-  test %(help! "message", error: false), out: "message\n\ntitle"
+  test %(help! "message", error: false), output: "message\n\ntitle"
 end
